55dc3a3dfc78f667ee823330c33b93d535d31607
KirstenMayland <kirsten.r.mayland.25@dartmouth.edu>
2024-06-28 19:30:44 -0400

    sheep acts normally
    well mostly normally, it starts jumping excitedly when you get too near

--- scripts/entities/sheep.gd
+++ scripts/entities/sheep.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const MAX_SPEED = 100.0
-#const JUMP_VELOCITY = -200.0
+@export var max_speed : float = 50.0
+@export var jump_velocity : float = -200.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,13 +12,31 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	var direction = get_direction_to_player()
-	set_velocity(direction * MAX_SPEED)
-	move_and_slide()
-
-func get_direction_to_player():
+	
 	var player_node = get_parent().get_node("Player")
 	if player_node != null:
-		return (player_node.global_position - global_position).normalized()
-	return Vector2.ZERO
+		var direction = (player_node.global_position - global_position).normalized()
+
+		# Horizontal movement
+		if direction.x != 0:
+			velocity.x = direction.x * max_speed
+			change_direction(direction.x)
+
+		# Jump if on the ground and close to the player
+		if is_on_floor() and abs(player_node.global_position.x - global_position.x) < 100:
+			velocity.y = jump_velocity
+	else: 
+		print("null")
+		set_velocity(Vector2.ZERO)
+	
+	move_and_slide()
+
+# ---------------------change_direction---------------------------
+# helper function for move; player faces direction it's moving
+func change_direction(direction):
+	if direction > 0:
+		get_node("AnimatedSprite2D").set_flip_h(false)
+	elif direction < 0:
+		get_node("AnimatedSprite2D").set_flip_h(true)
+		
+
