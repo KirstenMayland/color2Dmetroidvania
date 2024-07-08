175eaa8d0b5e185980e52c0c040b7cd25ba2c8a3
KirstenMayland <kirsten.r.mayland.25@dartmouth.edu>
2024-07-08 00:09:57 -0400

    story outline written
    + created hitbox component (but did nothing with it)

--- scripts/components/health_component.gd
+++ scripts/components/health_component.gd
@@ -1,8 +1,9 @@
+# handles health values and signaling changes
 extends Node2D
 class_name HealthComponent
 
 signal died
-signal hurt
+signal health_change(current_health: float, heal: bool)
 
 @export var max_health: float = 15
 var current_health
@@ -11,11 +12,23 @@ func _ready():
 	current_health = max_health
 
 func damage(damage_amount: float):
-	current_health = max(current_health - damage_amount, 0)
-	hurt.emit()
+	current_health = min(max(current_health - damage_amount, 0), max_health)
+	health_change.emit(current_health, damage_amount < 0)
 	Callable(check_death).call_deferred()
 
 func check_death():
 	if current_health == 0:
 		died.emit()
 		owner.queue_free()
+
+func heal(heal: float):
+	damage(-heal)
+
+func set_max_health(health: float):
+	max_health = health
+
+func initialize_health():
+	current_health = max_health
+
+func has_health_remaining():
+	return current_health > 0
--- scripts/components/hurtbox_component.gd
+++ scripts/components/hurtbox_component.gd
@@ -1,5 +1,8 @@
+# handles recieving damage
 extends Area2D
 class_name HurtboxComponent
 
-@export var health_component: Node
+@export var health_component: HealthComponent # used to be Node
 
+func can_accept_damage_collision():
+	return health_component.has_health_remaining()
