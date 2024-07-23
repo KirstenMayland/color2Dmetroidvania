580057df9fdd110aff7bf0f1c0dbc65d67ee74ae
KirstenMayland <kirsten.r.mayland.25@dartmouth.edu>
2024-07-23 17:29:54 -0400

    camera boundaries component basic
    need to fix scene changer bug

---
+++ scripts/components/camera_boundaries_component.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+# Node paths to the player and the camera
+@onready var player = get_parent().get_node("Player") as Player
+@onready var camera = player.get_node("Camera2D")
+
+# Rectangle representing the camera boundaries
+var camera_boundaries = Rect2()
+
+func _ready():
+	# Get the collision shape and set the boundaries based on its extents
+	var pos = $CollisionShape2D.global_position
+	var collision_shape = $CollisionShape2D.shape as RectangleShape2D
+	camera_boundaries = Rect2(pos - collision_shape.extents, collision_shape.extents * 2)
+
+func _process(delta):
+	# Ensure the camera stays within the boundaries
+	if player:
+		camera.limit_left = camera_boundaries.position.x
+		camera.limit_top = camera_boundaries.position.y
+		camera.limit_right = camera_boundaries.position.x + camera_boundaries.size.x
+		camera.limit_bottom = camera_boundaries.position.y + camera_boundaries.size.y
