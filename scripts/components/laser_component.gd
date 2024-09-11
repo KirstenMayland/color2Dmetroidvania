ad7e69f1a8bed2e36ce08fd6b793e6950d777765
KirstenMayland <kirsten.r.mayland.25@dartmouth.edu>
2024-09-11 13:50:41 -0400

    laser graphics done for now

--- scripts/components/laser_component.gd
+++ scripts/components/laser_component.gd
@@ -10,7 +10,9 @@ const max_range = 5000
 @onready var line = $Line2D
 @onready var hitbox = $Line2D/HitboxComponent
 @onready var collision_line = $Line2D/HitboxComponent/CollisionShape2D
+
 @onready var collision_sparks = $CollisionSparks
+@onready var charge_ani = $Charge
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -37,4 +39,6 @@ func _process(delta):
 		line.points[1] = collision_sparks.global_position
 		collision_line.shape.b = collision_sparks.global_position
 	
+	# start and end rotate with line
 	collision_sparks.rotation = line.points[1].angle()
+	charge_ani.rotation = line.points[1].angle()
