60712b361ac7dce392e45adcbb42ffd0562464b7
KirstenMayland <kirsten.r.mayland.25@dartmouth.edu>
2024-09-12 01:29:30 -0400

    more laser updates, all functional

--- scripts/components/laser_component.gd
+++ scripts/components/laser_component.gd
@@ -1,31 +1,59 @@
 extends Node2D
+class_name LaserComponent
 
 const max_range = 5000
 
-@export var based_width: float = 15
-@export var speed: float = 100
+# beam modifications variables
+@export var based_width: float = 64
+@export var speed: float = 20
 @export var beam_dist_from_center: float = 6
 @export var damage_dealt: float = 10
+
+# beam usage variables
 @export var active: bool = false
 
+# beam path variables
 @export var start_point: Marker2D
 @export var end_point: Marker2D
+@onready var path_position: Vector2
 
-@onready var path_position: Vector2 = start_point.global_position
+var x_increment: float
+var y_increment: float
+var x_direction: int
+var y_direction: int
 
+# references to subcomponents
+# TODO: make Line2D texture and default color + charge sprite + collision sparks sprite exports
 @onready var ray_cast = $RayCast2D
 @onready var line = $Line2D
 @onready var hitbox = $Line2D/HitboxComponent
 @onready var collision_line = $Line2D/HitboxComponent/CollisionShape2D
-
 @onready var collision_sparks = $CollisionSparks
 @onready var charge_ani = $Charge
+
+# signals
+signal laser_destination_reached
 
 # ----------------------------------------------------------------
 # -----------------------------ready------------------------------
 # ----------------------------------------------------------------
 func _ready():
+	if start_point and end_point:
+		path_pre_calculations(start_point.global_position, end_point.global_position)
 	hitbox.set_damage_dealt(damage_dealt)
+
+
+func path_pre_calculations(start: Vector2, end: Vector2):
+	path_position = start
+	
+	# calculate ratio to move in
+	x_increment = (end.x - start.x) / 100
+	y_increment = (end.y - start.y) / 100
+	
+	# calculate relative positions
+	x_direction = -1 if (end.x - start.x) < 0 else 1
+	y_direction = -1 if (end.y - start.y) < 0 else 1
+
 
 # ----------------------------------------------------------------
 # ----------------------_physics_process--------------------------
@@ -37,52 +65,62 @@ func _physics_process(delta):
 	if active:
 		set_visible(true)
 		collision_line.set_disabled(false)
+		
 		if start_point and end_point:
-			move_between_points(start_point.global_position, end_point.global_position, speed, delta)
+			move_between_points(end_point.global_position, delta)
 			update_angles_and_positions()
+		
 	else:
 		set_visible(false)
 		collision_line.set_disabled(true)
 
 
-func move_between_points(start: Vector2, end: Vector2, speed, delta):
-	if path_position == null:
-		path_position = start
-	
-	# calculate ratio to move in
-	var x_increment = (end.x - start.x) / 100
-	var y_increment = (end.y - start.y) / 100
-	
+func move_between_points(end: Vector2, delta):
 	# move along path
-	if path_position.x < end.x and path_position.y < end.y:
+	if (path_position.x * x_direction) < (end.x * x_direction) and (path_position.y * y_direction) < (end.y * y_direction):
 		path_position.x += x_increment * speed * delta
 		path_position.y += y_increment * speed * delta
+	else:
+		emit_signal("laser_destination_reached")
 	
 	# convert to local coords, extend to go until walls, and set as target
 	var max_cast_to = to_local(path_position).normalized() * max_range
 	ray_cast.target_position = max_cast_to
 
+
 func update_angles_and_positions():
 	if ray_cast.is_colliding():
+		# put collision art at collision
 		collision_sparks.global_position = ray_cast.get_collision_point()
-		print(collision_sparks.global_position.x)
 		
-		line.set_point_position(0, Vector2(beam_dist_from_center*cos(line.points[1].angle()), beam_dist_from_center*sin(line.points[1].angle()) ))
+		# set end of beam to collision
 		line.set_point_position(1, line.to_local(collision_sparks.global_position))
 		
+		# update collision shape as well
 		collision_line.shape.b = line.to_local(collision_sparks.global_position)
 	else:
+		# put collision art at the end of the beam
 		collision_sparks.global_position = ray_cast.target_position
 		
+		# set end of beam and collision shape to end where it should
 		line.points[1] = collision_sparks.global_position
 		collision_line.shape.b = collision_sparks.global_position
+	
+	# move start of beam away from center
+	line.set_point_position(0, Vector2(beam_dist_from_center*cos(line.points[1].angle()), beam_dist_from_center*sin(line.points[1].angle()) ))
 	
 	# start and end rotate with line
 	collision_sparks.rotation = line.points[1].angle()
 	charge_ani.rotation = line.points[1].angle()
+
 
 func activate_laser():
 	active = true
 
 func deactivate_laser():
 	active = false
+
+
+# TODO: create fire_up_laser function
+func fire_up_laser():
+	pass
--- scripts/entities/grove_guardian.gd
+++ scripts/entities/grove_guardian.gd
@@ -2,15 +2,27 @@ extends CharacterBody2D
 
 @export var max_speed : float = 50.0
 
+# references to subcomponents
 @onready var animation = $AnimationPlayer
 @onready var health_component: HealthComponent = $HealthComponent
 @onready var direction_component: DirectionComponent = $DirectionComponent
+@export var laser_one: LaserComponent
+@export var laser_two: LaserComponent
+
+# finite state machine variables
+@onready var in_idle_state = true
+var in_follow_state = false
+var in_laser_state = false
+var in_death_state = false
 
 # ----------------------------------------------------------------
 # -----------------------------ready------------------------------
 # ----------------------------------------------------------------
 func _ready():
 	health_component.died.connect(on_death)
+	health_component.health_change.connect(on_hurt)
+	
+	laser_one.laser_destination_reached.connect(laser_reached_destination)
 
 func on_death():
 	queue_free()
@@ -18,9 +30,37 @@ func on_death():
 # ----------------------------------------------------------------
 # ---------------------_physics_process---------------------------
 # ----------------------------------------------------------------
-func _physics_process(delta):
-	update_animation_parameters(delta)
-	idle_state()
+func _physics_process(_delta):
+	finite_state_machine()
+
+
+# right now, starts as idle, until it is hurt
+# it then switches to laser
+# and once the laser is done, it switches to follow
+
+func finite_state_machine():
+	if in_idle_state:
+		idle_state()
+	elif in_follow_state:
+		follow_state()
+	elif in_laser_state:
+		laser_state()
+	elif in_death_state:
+		death_state()
+
+# TODO: unfortunately no pointers, so figure out a way to cleanly change between states without looping
+
+func idle_state():
+	animation.play("Idle")
+
+func on_hurt(heal):
+	in_idle_state = false
+	in_laser_state = true
+
+
+func follow_state():
+	animation.play("Idle")
+	move(direction_component.get_direction_vector_to_player())
 
 func move(direction):
 	# Horizontal movement
@@ -34,20 +74,19 @@ func move(direction):
 	move_and_slide()
 
 
-func update_animation_parameters(_delta):
-	animation.play("Idle")
-
-func finite_state_machine():
-	pass
-
-func idle_state():
-	move_and_slide()
-
-func follow_state():
-	move(direction_component.get_direction_vector_to_player())
-
 func laser_state():
-	pass
+	animation.play("Idle")
+	# TODO: when lasers are first activated, have charge period
+	laser_one.activate_laser()
+	laser_two.activate_laser()
 
+func laser_reached_destination():
+	laser_one.deactivate_laser()
+	laser_two.deactivate_laser()
+	in_laser_state = false
+	in_follow_state = true
+
+
+# should probably have on_death signal go straight here
 func death_state():
 	pass
